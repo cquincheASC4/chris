4856fd2a25f03f9b182d9a4eba0b411978eb3649
cquincheASC4 <quinchechrist@gmail.com>
2017-07-13 12:04:52 -0400

    new thurs project

---
+++ pokemon-roster.js
@@ -0,0 +1,19 @@
+var pokemonRoster = [
+    ["Arcanine", "Fire", 90, 80, 110, false],
+    ["Alakazam", "Psychic", 55, 45, 50, false],
+    ["Mewto", "Psychic", 106, 90, 110, true],
+    ["Wailord", "Water", 170, 45, 90, false],
+    ["Deoxys", "Psychic", 50, 50, 150, true],
+    ["Garchomp", "Dragon and Ground", 108, 95, 130, false]
+];
+
+function specificPokemonStats() {
+    for(var i = 0; i < pokemonRoster.length ; i++)
+        console.log(pokemonRoster[0])
+        console.log(pokemonRoster[1])
+        console.log(pokemonRoster[2])
+        console.log(pokemonRoster[3])
+        console.log(pokemonRoster[4])
+        console.log(pokemonRoster[5])
+}
+specificPokemonStats()

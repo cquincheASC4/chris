d8e8b85fcb5feedb19159abf81819f0595616c0b
cquincheASC4 <quinchechrist@gmail.com>
2017-08-09 15:57:48 -0400

    new files

--- chris-highscore/main.js
+++ chris-highscore/main.js
@@ -1,38 +1,83 @@
-var choices = ["rock", "scissors", "paper"];
-var randomChoice = Math.floor(Math.random() * 3);
 
 
-$('.rock').click(function() {
-  p1Choice = choices[0];
-    var p2Choice = choices[randomChoice];
-if(p1Choice == "rock" && p2Choice == "scissors") {
-    $('body').append("<p>Player one wins</p>");
-}else if (p1Choice == "rock" && p2Choice == "paper") {
-    $('body').append("<p>Player two wins</p>")
-}else {
-    $('body').append("<p>It's a tie!</p>");
-}
-})
+var rps = ["rock", "paper", "scissors"];
+var randomNum = Math.floor(Math.random()*3);
+var randomChoice = rps[randomNum];
+var userScore = 0;
+function chooseChoice(value) {
+        if(value == "Rock" && randomChoice == "paper") {
+            $("#results").html('').append("<p> The computer chose " + randomChoice + ". You lose.</p>");
+            userScore = userScore - 1;
+            $("#score").html('').append(userScore);
+        }
+        else if(value == "Rock" && randomChoice == "rock") {
+            $("#results").html('').append("<p> The computer chose " + randomChoice + ". It's a tie.</p>");
+            userScore = userScore + 0;
+            $("#score").html('').append(userScore);
+        }
+        else if(value == "Rock" && randomChoice == "scissors") {
+            $("#results").html('').append("<p> The computer chose " + randomChoice + ". You win.</p>");
+            userScore = userScore + 1;
+            $("#score").html('').append(userScore);
+        }
+        else if(value == "Paper" && randomChoice == "paper") {
+            $("#results").html('').append("<p> The computer chose " + randomChoice + ". It's a tie.</p>");
+            userScore = userScore + 0;
+            $("#score").html('').append(userScore);
+        }
+        else if(value == "Paper" && randomChoice == "scissors") {
+            $("#results").html('').append("<p> The computer chose " + randomChoice + ". You lose.</p>");
+            userScore = userScore - 1;
+            $("#score").html('').append(userScore);
+        }
+        else if(value == "Paper" && randomChoice == "rock") {
+            $("#results").html('').append("<p> The computer chose " + randomChoice + ". You win.</p>");
+            userScore = userScore + 1;
+            $("#score").html('').append(userScore);
+        }
+        else if(value == "Scissors" && randomChoice == "rock") {
+            $("#results").html('').append("<p> The computer chose " + randomChoice + ". You lose.</p>");
+            userScore = userScore - 1;
+            $("#score").html('').append(userScore);
+        }
+        else if(value == "Scissors" && randomChoice == "paper") {
+            $("#results").html('').append("<p> The computer chose " + randomChoice + ". You win.</p>");
+            userScore = userScore + 1;
+            $("#score").html('').append(userScore);
+        }
+        else {
+            $("#results").html('').append("<p> The computer chose " + randomChoice + ". It's a tie.</p>");
+            userScore = userScore + 0;
+            $("#score").html('').append(userScore);
+        }
+    randomNum = Math.floor(Math.random()*3);
+    randomChoice = rps[randomNum];
+    
 
-$('.paper').click(function() {
-     p1Choice = choices[2];
-    var p2Choice = choices[randomChoice];
-if (p1Choice == "paper" && p2Choice == "rock") {
-    console.log(" Player one wins");
-}else if(p1Choice == "paper" && p2Choice == "scissors") {
-    console.log(" Player two wins");
-}else{
-    console.log( "It's a tie");
 }
-})
-$('.scissors').click(function() {
-     p1Choice = choices[1];
-    var p2Choice = choices[randomChoice];
-if (p1Choice == "scissors" && p2Choice == "paper") {
-    console.log(" Player one wins");
-}else if (p1Choice == "scissors" && p2Choice == "rock") {
-    console.log(" Player two wins");
-}else{
-    console.log( "It's a tie");
+
+
+// //Set database object
+var database = firebase.database().ref()
+
+//button executes this function
+function updateDB(){
+    var name = $("#name").val();
+    console.log(name);
+
+    
+    var saveScore = userScore;
+    console.log(saveScore);
+
+    var value = {
+        "NAME" : name,
+        "SCORE" : saveScore,
+        
+    }
+    //Update database here
+database.push(value);
 }
+
+database.on("child_added", function(rowData) {
+    $("#leaderBoard").append("<div>Name: " + rowData.val().NAME  + " Score: " + rowData.val().SCORE + "</div>")
 })

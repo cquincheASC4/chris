0553f659a771fb5ae9ab38e3b3312238ac59985f
cquincheASC4 <quinchechrist@gmail.com>
2017-08-15 23:34:23 -0400

    updated game files

--- demoDayProject/Bocor/js/gameCode.js
+++ demoDayProject/Bocor/js/gameCode.js
@@ -1,10 +1,11 @@
 //Set database object
 var database = firebase.database().ref();
-function setup() {
+
 // creating the random word generator
     var words=['conclusion','tooth','bedroom','opinion','math','engine','protection','courage','hair','celebration','reaction','assistance','desk','feedback','trainer','procedure','surgery','complaint','presentation','passion','message','actor','paper','law','basket','person','resolution','operation','police','manager','grandmother','goal','education','knowledge','director','mixture','equipment','activity','shopping','championship','midnight','football','elevator','arrival','data','direction','judgment','indication','professor','drama','reputation','moment','height','entry','farmer','population','device','singer','potato','sample','perspective','way','finding','version','church','member','event','wedding','extent','theory','opportunity','engineering','relationship','platform','article','anxiety','depression','negotiation','orange','reflection','computer','recipe','ladder','construction','cousin','injury','concept','unit','customer','efficiency','piano','girlfriend','addition','city','region','awareness','appearance','consequence','variation','topic']
     var rand = Math.floor(Math.random()*words.length);
     var randwords= words[rand]
+function setup() {
     console.log(randwords)
     $('.word').append("<p>Your word is: " + randwords + "</p>")
 
@@ -16,7 +17,7 @@ function getTimeRemaining(endtime) {
   var seconds = Math.floor((t / 1000) % 60);
   var minutes = Math.floor((t / 1000 / 60) % 60);
 
- return {
+  return {
     'total': t,
     'minutes': minutes,
     'seconds': seconds
@@ -28,13 +29,13 @@ function initializeClock(id, endtime) {
   var minutesSpan = clock.querySelector('.minutes');
   var secondsSpan = clock.querySelector('.seconds');
 
- function updateClock() {
+  function updateClock() {
     var t = getTimeRemaining(endtime);
 
-   minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
     secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
-   if (t.total <= 0) {
+    if (t.total <= 0) {
         clearInterval(timeinterval);
         $('.timer').append(`<h1> TIME IS UP!</h1>`);
         alert("Your time is up and your story looks great! Would you like to view it?")
@@ -45,11 +46,11 @@ function initializeClock(id, endtime) {
     }
   }
 
- updateClock();
+  updateClock();
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = new Date(Date.parse(new Date()) +  /*180 * */  7000);
+var deadline = new Date(Date.parse(new Date()) +  /*60 * */  8000);
 initializeClock('clockdiv', deadline);
 
 }
@@ -59,34 +60,37 @@ function wordInString(s, word){
       alert("OH!It seems you didnt use the word. Looks like you are going to have to restart 3:) .");
       location.reload(true);
   }
+//updateDB()
+}
+
+console.log(randwords)
+
+$(document).ready(setup);
 
 
-var database = firebase.database().ref()
 //button executes this function
 function updateDB(){
-    var name = user.displayName
-    // var email= user.email;
+
+   // var email= user.email;
     var story = $('#story').val()
-    console.log(name + " : " + story);
+    var word = randwords
+    console.log(word + " : " + story);
 
 
-   var value={
-        // "EMAIL": email,
+    var value={
+       // "EMAIL": email,
+        "WORD": word,
         "STORY": story,
     }
 
-   //Update database here
+    //Update database here
     database.push(value);
 
-}
 
 database.on('child_added', function(rowData) {
     var row = rowData.val();
-    // var email = row.EMAIL;
+    var word= row.WORD
+   // var email = row.EMAIL;
     var story = row.STORY;
     //$('.allMessages').append('<p>'+name +": "+ message+'</p>');
-})
-}
-
-
-$(document).ready(setup);
+})}
